e7c580d9dd36a296f006ecc635c37b09ce9832f9
leonardo-eyer <leonardo.eyer@hotmail.com>
2025-08-04 15:49:25 -0300

    Starting project

---
+++ main.py
@@ -0,0 +1,16 @@
+import pygame
+
+pygame.init()
+WINDOW_WIDTH, WINDOW_HEIGHT = 1280, 720
+display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+running = True
+
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    display_surface.fill("blue")
+    pygame.display.update()
+
+pygame.quit()
